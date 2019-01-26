d653c9f7be34d996036daf75f88ae7fd386f5c11
j33ty <31261252+j33ty@users.noreply.github.com>
2019-01-27 04:02:41 +0530

    Reverse Commit loop; Modify Printfs; Fix date formatting

--- gitstar.sh
+++ gitstar.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-git_remote="git@github.com:username/repo.git"
 git_user_name="username"
-git_user_email="lulz@email.com"
-end_date="24-01-19"
+git_user_email="email@email.com"
+start_date="2018-02-21" # Year-Month-Day
 
 # weekday rules
 weekday_commit_min=0
@@ -33,46 +32,46 @@ get_random_num() {
 # create_commit $Year-Month-Day #Count
 create_commit() {
 	echo "$2 on $1" >> commit.md
-	hour=$(get_random_num 10 20)
-	minute=$(get_random_num 10 59)
-	second=$(get_random_num 10 59)
+	local hour=$(get_random_num 10 20)
+	local minute=$(get_random_num 10 59)
+	local second=$(get_random_num 10 59)
 	export GIT_COMMITTER_DATE="$1 $hour:$minute:$second"
 	export GIT_AUTHOR_DATE="$1 $hour:$minute:$second"
 	git add commit.md -f
-	git commit --date="$1 $hour:$minute:$second" -m "$2 on $1"
+	git commit --date="$1 $hour:$minute:$second" -m "$2 on $1" >/dev/null
 }
 
 run_gitsploit() {
-	git clone $git_remote; cd gitsploit
+	mkdir gitsploit; cd gitsploit
+	git init
 	git config user.name $git_user_name
 	git config user.email $git_user_email
-	newDate=$(date '+%y-%m-%d')
-	counter=1
-	while [ "$newDate" != $end_date ]; do
-		newDate=$(date -v -${counter}d '+%d-%m-%y')
-		counter=$((counter + 1))
+	local end_date=$(date '+%Y-%m-%d')
+	local commit_date=$start_date
+	local counter=1
+	while [ "$commit_date" != $end_date ]; do
 		local limit
-		if [[ $(date -j -f "%d-%m-%y" $newDate "+%u") -lt 6 ]] ; then
+		if [[ $(date -j -f "%Y-%m-%d" $commit_date "+%u") -lt 6 ]] ; then
 			limit=$(get_random_num $weekday_commit_min $weekday_commit_max $weekday_rest_allowed)
 		else
 			limit=$(get_random_num $weekend_commit_min $weekend_commit_max $weekend_rest_allowed)
 		fi
 
 		for i in $(seq 1 $limit); do
-			create_commit $newDate $i
+			printf "\nOn Date: $commit_date"
+			create_commit $commit_date $i
 		done
+		commit_date=$(date -j -v +${counter}d -f "%Y-%m-%d" "$start_date" "+%Y-%m-%d")
+		counter=$((counter + 1))
 	done
-	git push origin master
 }
 
 cleanup() {
-	echo "Unsetting GIT_COMMITTER_DATE: " $GIT_COMMITTER_DATE
+	printf "\nUnsetting Vars and Cleaning up!"
 	unset GIT_COMMITTER_DATE
-	echo "Unsetting GIT_AUTHOR_DATE: " $GIT_COMMITTER_DATE
 	unset GIT_AUTHOR_DATE
-	git rm commit.md
-	git commit -am "cleanup"
-	git push origin master
+	git rm commit.md >/dev/null
+	git commit -am "Cleanup" >/dev/null
 }
 
 on_err() {
@@ -80,7 +79,7 @@ on_err() {
 }
 
 on_exit() {
-	printf "\nDone making you a Gitstar!"
+	printf "\nDone making you a Gitstar! Now, Set your remote repo and Push the repo using 'git push origin master'"
 }
 
 run_gitsploit
